866b1b63a63788df1e8108afab6fbf8e2f4ab0ea
Pedram Mirelmi <82533743+Pedram-Mirelmi@users.noreply.github.com>
2021-05-12 03:35:07 +0430

    create a simple tester file
    it shows the a simple window including several tweets in a scroll area
    the original project is something like this file

---
+++ client/UI_packages/Widgets/tester.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import QWidget, QGridLayout, QLabel, \
+    QTextBrowser, QPushButton, QScrollArea, QApplication, QMainWindow
+
+from singleTweet import SingleTweetBox
+from ManyTweet import ManyTweetBox
+
+
+class MyWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.tweet_menu = ManyTweetBox()
+        self.setAutoFillBackground(True)
+        for i in range(20):
+            self.tweet_menu.t_container.grid.addWidget(SingleTweetBox(), i, 0, 1, 1)
+        self.setCentralWidget(self.tweet_menu)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    win = MyWindow()
+    win.show()
+
+    sys.exit(app.exec_())
