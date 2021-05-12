bf226445ba3f7c1221e3ec82933fe025c7b384d3
Pedram Mirelmi <82533743+Pedram-Mirelmi@users.noreply.github.com>
2021-05-13 03:32:57 +0430

    Create Keywords for client part

---
+++ client/BackendPackages/ClientKeywords.py
@@ -0,0 +1,42 @@
+MARK_SIGN, TRUE, FALSE = "✔", "TRUE", "FALSE"
+USERNAME, PASSWORD, NAME = "username", "password", "name"
+USER_ID = "user_id"
+CREATED_AT = "created_at"
+# Necessary table names
+TWEETS = 'tweets'
+USERS = 'users'
+COMMENTS = 'comments'
+
+E1 = 'Wrong password!'
+E2 = 'Wrong username'
+
+E_CODE = 'error_code'
+REQUEST_TYPE = "requestType"
+OUTCOME = "outcome"
+LOGIN = 'login'
+REGISTER = 'register'
+STATUS = 'status'
+
+NEW_TWEET = 'new_tweet'
+TWEET_TEXT = 'tweet_text'
+ALL_TWEETS = 'all_tweets'
+TWEET_ID = 'tweet_id'
+LIKE_TWEET = 'like_tweet'
+GET_COMMENTS = 'get_comments'
+
+LIKES = 'likes'
+ADD_COMMENT = 'add_comment'
+COMMENT_TEXT = 'comment_text'
+
+
+SELECT = 'SELECT'
+INSERT = 'INSERT'
+UPDATE = 'UPDATE'
+INTO = 'INTO'
+VALUES = 'VALUES'
+SET = 'SET'
+FROM = 'FROM'
+WHERE = 'WHERE'
+JOIN = 'JOIN'
+ON = 'ON'
+USING = 'USING'
