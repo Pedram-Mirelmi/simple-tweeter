54f7cb44c1fae18446001c115e32a10958d35ee7
Pedram Mirelmi <82533743+Pedram-Mirelmi@users.noreply.github.com>
2021-05-14 03:15:27 +0430

    Update ManyTweet.py

--- client/UIPackages/Widgets/ManyTweet.py
+++ client/UIPackages/Widgets/ManyTweet.py
@@ -1,5 +1,12 @@
+import sys
+
+sys.path.insert(1, '/home/pedram/PycharmProjects/my-project/')
+sys.path.insert(1, '/home/pedram/PycharmProjects/my-project/client')
+
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QScrollArea, QGridLayout, QApplication
+from .WriteTweet import WriteTweetBox
+from .SingleTweet import SingleTweetBox
 
 
 class ManyTweetContainer(QWidget):
@@ -10,16 +17,32 @@ class ManyTweetContainer(QWidget):
 
 
 class ManyTweetBox(QScrollArea):
+    tweetCatcher: callable
     def __init__(self, mother_area: QWidget = None):
         super().__init__(mother_area)
-        self.resize(652, 880)
-        self.setGeometry(QtCore.QRect(11, 10, 631, 861))
-        self.setWidgetResizable(True)
-        self.setObjectName("scrollArea")
         self.t_container = ManyTweetContainer()
-        self.grid = QGridLayout(self)
-        self.grid.addWidget(self.t_container)
         self.setWidget(self.t_container)
+        self.setWidgetResizable(True)
+        self.row_index = 1
+
+    def updateAllTweets(self):
+        self.row_index = 1
+        all_tweets = self.tweetCatcher()
+        for tweet in all_tweets:
+            self.addSingleTweet(tweet)
+
+    def addWriteTweetHeader(self, username: str):
+        self.header = WriteTweetBox(mother_area=self.t_container, username=username)
+        self.t_container.grid.addWidget(self.header, 0, 0, 1, 1)
+
+    def addProfileInfoHeader(self, profile_info: dict[str, str]):  # TODO
+        pass
+
+    def addSingleTweet(self, tweet_info: dict[str, str]):
+        new_tweet = SingleTweetBox(self.t_container)
+        new_tweet.initiateTweet(tweet_info)
+        self.t_container.grid.addWidget(new_tweet, self.row_index, 0, 1, 1)
+        self.row_index += 1
 
 
 if __name__ == "__main__":
