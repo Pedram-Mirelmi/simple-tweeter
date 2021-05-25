fe81cd3363f160a9af7441bba621ec880a789ead
Pedram Mirelmi <82533743+Pedram-Mirelmi@users.noreply.github.com>
2021-05-25 18:01:39 +0430

    Update ManyTweet.py

--- client/UIPackages/Widgets/ManyTweet.py
+++ client/UIPackages/Widgets/ManyTweet.py
@@ -1,8 +1,3 @@
-import sys
-
-sys.path.insert(1, '/home/pedram/PycharmProjects/my-project/')
-sys.path.insert(1, '/home/pedram/PycharmProjects/my-project/client')
-
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QWidget, QScrollArea, QGridLayout, QApplication
 from .WriteTweet import WriteTweetBox
@@ -18,28 +13,31 @@ class ManyTweetContainer(QWidget):
 
 class ManyTweetBox(QScrollArea):
     tweetCatcher: callable
-    def __init__(self, mother_area: QWidget = None):
+
+    def __init__(self, mother_area: QWidget = None, reloader: callable = None):
         super().__init__(mother_area)
+        self.reloader = reloader
         self.t_container = ManyTweetContainer()
         self.setWidget(self.t_container)
         self.setWidgetResizable(True)
         self.row_index = 1
 
-    def updateAllTweets(self):
-        self.row_index = 1
+    def updateAllTweets(self, all_tweets: list[dict[str, str]] = None):
         all_tweets = self.tweetCatcher()
+        self.row_index = 1
         for tweet in all_tweets:
             self.addSingleTweet(tweet)
 
     def addWriteTweetHeader(self, username: str):
-        self.header = WriteTweetBox(mother_area=self.t_container, username=username)
+        self.header = WriteTweetBox(self.t_container, self.updateAllTweets)
+        self.header.initiateBox(username)
         self.t_container.grid.addWidget(self.header, 0, 0, 1, 1)
 
     def addProfileInfoHeader(self, profile_info: dict[str, str]):  # TODO
         pass
 
     def addSingleTweet(self, tweet_info: dict[str, str]):
-        new_tweet = SingleTweetBox(self.t_container)
+        new_tweet = SingleTweetBox(self.t_container, self.updateAllTweets)
         new_tweet.initiateTweet(tweet_info)
         self.t_container.grid.addWidget(new_tweet, self.row_index, 0, 1, 1)
         self.row_index += 1
