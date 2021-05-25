0e4cb1bb7783e41fd04d8db594a976cedb9192e0
Pedram Mirelmi <82533743+Pedram-Mirelmi@users.noreply.github.com>
2021-05-25 18:03:32 +0430

    Update WriteTweet.py

--- client/UIPackages/Widgets/WriteTweet.py
+++ client/UIPackages/Widgets/WriteTweet.py
@@ -1,29 +1,32 @@
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QGroupBox, QWidget, QGridLayout, QPlainTextEdit, QPushButton, QApplication
+from PyQt5.QtWidgets import QGroupBox, QWidget, QGridLayout, QPushButton, QTextEdit
 
 
 class WriteTweetBox(QGroupBox):
     sendButtonFunc: callable
 
-    def __init__(self, username: str, mother_area: QWidget = None):
+    def __init__(self, mother_area: QWidget = None, reloader: callable = None):
         super().__init__(mother_area)
+        self.reloader = reloader
         self.setGeometry(QtCore.QRect(270, 40, 450, 244))
         self.setMinimumSize(QtCore.QSize(450, 244))
         self.setMaximumSize(QtCore.QSize(450, 250))
-        self.username = username
-        self.__initiateBox()
 
-    def __initiateBox(self):
+    def initiateBox(self, username):
         self.gridLayout = QGridLayout(self)
-        self.tweet_text_field = QPlainTextEdit(self)
+        self.tweet_text_field = QTextEdit(self)
         self.gridLayout.addWidget(self.tweet_text_field, 0, 0, 1, 1)
         self.send_button = QPushButton(self)
         self.gridLayout.addWidget(self.send_button, 1, 0, 1, 1)
-        self.__initiateTexts()
-        self.send_button.clicked.connect(
-            lambda: self.sendButtonFunc(self.tweet_text_field.text)
-        )
+        self.__initiateTexts(username)
+        self.send_button.clicked.connect(self.sendFunc)
 
-    def __initiateTexts(self):
-        self.setTitle(f"Write Tweet as '{self.username}':")
+    def sendFunc(self):
+        if self.tweet_text_field.toPlainText().strip():
+            self.sendButtonFunc(self.tweet_text_field.toPlainText().strip())
+            self.reloader()
+        self.tweet_text_field.clear()
+
+    def __initiateTexts(self, username: str):
+        self.setTitle(f"Write Tweet as '{username}':")
         self.send_button.setText("Send")
